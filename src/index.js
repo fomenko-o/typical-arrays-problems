44c64755f65bed75e2a54f0a6108251c06f2d213
Oleksandra Fomenko <oleksandra.fomenko@gmail.com>
2020-10-03 16:52:23 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+ if(!array || array.includes() || array.length == 0) { return 0 };
+ return array.sort((a, b) => a - b)[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.includes() || array.length == 0) { return 0 };
+  return array.sort((a, b) => a - b)[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.includes() || array.length == 0) { return 0 };
+  return array.reduce((a, b) => a + b)/array.length;
 }
